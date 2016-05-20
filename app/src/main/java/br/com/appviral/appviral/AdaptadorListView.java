552b7b9e03d7b3480f734ac6aa9d6aabd7c1f1eb
Martin Morães <martin.moraes@gmail.com>
2016-05-20 10:48:31 -0300

    Implementado o getView do Adaptador.

--- app/src/main/java/br/com/appviral/appviral/AdaptadorListView.java
+++ app/src/main/java/br/com/appviral/appviral/AdaptadorListView.java
@@ -1,8 +1,14 @@
 package br.com.appviral.appviral;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -13,7 +19,15 @@ import br.com.appviral.appviral.Entidade.DadosDoPost;
  */
 public class AdaptadorListView extends BaseAdapter {
 
-    ArrayList<DadosDoPost> listaDeDados = new ArrayList<>();
+    ArrayList<DadosDoPost> listaDeDados;
+    LayoutInflater layoutInflater;
+    Context context;
+
+    public AdaptadorListView(Context context){
+        this.context = context;
+        listaDeDados = new ArrayList<>();
+        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
 
 
     @Override
@@ -34,6 +48,30 @@ public class AdaptadorListView extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+
+        if(convertView == null){
+            convertView = layoutInflater.inflate(R.layout.layout_item_listview, null);
+        }
+
+        ImageView imageView = (ImageView) convertView.findViewById(R.id.imageView);
+        TextView texto = (TextView) convertView.findViewById(R.id.textView);
+
+        //Povoar
+        DadosDoPost post = listaDeDados.get(position);
+        texto.setText(post.texto);
+        int img = context.getResources().getIdentifier(post.imagemURL,"mipmap",context.getPackageName());
+        imageView.setImageResource(img);
+        return convertView;
     }
+
+
+
+
+
+
+
+
+
+
+
 }
